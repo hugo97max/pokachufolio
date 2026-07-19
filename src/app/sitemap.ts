07846977c987bcf47ec63ece505492d919ec535d
Pokachu <hugo97max@gmail.com>
2026-07-19 15:40:21 -0500

    Respect disabled routes in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,15 +2,19 @@ import { getPosts } from "@/app/utils/utils";
 import { baseURL, routes as routesConfig } from "@/app/resources";
 
 export default async function sitemap() {
-  const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
-    url: `https://${baseURL}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+  const blogs = routesConfig["/blog"]
+    ? getPosts(["src", "app", "blog", "posts"]).map((post) => ({
+        url: `https://${baseURL}/blog/${post.slug}`,
+        lastModified: post.metadata.publishedAt,
+      }))
+    : [];
 
-  const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({
-    url: `https://${baseURL}/work/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+  const works = routesConfig["/work"]
+    ? getPosts(["src", "app", "work", "projects"]).map((post) => ({
+        url: `https://${baseURL}/work/${post.slug}`,
+        lastModified: post.metadata.publishedAt,
+      }))
+    : [];
 
   const activeRoutes = Object.keys(routesConfig).filter((route) => routesConfig[route]);
 
