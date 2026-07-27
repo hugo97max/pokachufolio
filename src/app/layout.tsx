cf09510e723409a415b2bab95187d9761ffb1d6f
Pokachu <hugo97max@gmail.com>
2026-07-26 20:13:39 -0500

    Usa fuente local en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,7 @@ import classNames from "classnames";
 import { Footer, Header, RouteGuard } from "@/components";
 import { baseURL, effects, style } from "@/app/resources";
 
-import { Inter } from "next/font/google";
-import { Source_Code_Pro } from "next/font/google";
+import localFont from "next/font/local";
 
 import { person, home } from "@/app/resources/content";
 import { Background, Column, Flex, ToastProvider } from "@/once-ui/components";
@@ -39,9 +38,9 @@ export async function generateMetadata() {
   };
 }
 
-const primary = Inter({
+const primary = localFont({
+  src: "../../public/fonts/Inter.ttf",
   variable: "--font-primary",
-  subsets: ["latin"],
   display: "swap",
 });
 
@@ -58,9 +57,9 @@ const tertiary: FontConfig | undefined = undefined;
 /*
  */
 
-const code = Source_Code_Pro({
+const code = localFont({
+  src: "../../public/fonts/Inter.ttf",
   variable: "--font-code",
-  subsets: ["latin"],
   display: "swap",
 });
 
